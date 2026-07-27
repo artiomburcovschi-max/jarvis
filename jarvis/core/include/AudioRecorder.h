17ea016e823d646512635a674edb4eb1357b687f
Artiom Burcovschi <artiomburcovschi@gmail.com>
2026-07-27 14:43:26 +0300

    Update AudioRecorder.h

--- jarvis/core/include/AudioRecorder.h
+++ jarvis/core/include/AudioRecorder.h
@@ -11,15 +11,10 @@ public:
     AudioRecorder();
     ~AudioRecorder();
 
-    // Запуск записи. Можно передать подстроку имени ("pulse", "picun", "default")
     bool Start(const std::string& deviceNameHint = "default");
     void Stop();
     bool ReadFrame(std::vector<int16_t>& frame);
 
-    // Вспомогательный метод для поиска ID по имени.
-    // ВАЖНО: предполагает, что Pa_Initialize() уже вызван (см. Start()) -
-    // сам не инициализирует и не терминирует PortAudio, чтобы не плодить
-    // несбалансированные Pa_Initialize()/Pa_Terminate() при внутреннем вызове.
     static int FindDeviceIdByName(const std::string& nameSubstr);
 
 private:
